7bc21e88b1a3eb9af7fcfeee55b75a424edf4deb
Shannon <115729658+damnsemicolon@users.noreply.github.com>
2023-03-09 17:43:12 +0000

    added Footer.jsx and structure

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import Analytics from './components/Analytics';
 import Newsletter from './components/Newsletter';
 import Cards from './components/Cards';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
-      { /*<Footer /> */}
+      <Footer />
     </div>
   );
 }
