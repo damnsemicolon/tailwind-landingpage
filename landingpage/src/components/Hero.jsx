58ad4f470b32e8184732d4d8cb6f81e477cc3016
Shannon <115729658+damnsemicolon@users.noreply.github.com>
2023-03-09 16:58:12 +0000

    added responsiveness

--- landingpage/src/components/Hero.jsx
+++ landingpage/src/components/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
                 <div className='flex justify-center items-center'>
                     <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
+                    {/* Adding fancy effect here - *react-typed* */}
                     <Typed
                         className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2.5'
                         strings={['B2B', 'B2C', 'SASS']}
